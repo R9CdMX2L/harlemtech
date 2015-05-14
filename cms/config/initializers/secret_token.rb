338f37da5e930de7fa55b9b5dc929da24f76a5b7
topcoder <support@topcoder.com>
2015-05-14 11:42:20 +0200

    first commit

---
+++ cms/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AcmeCms::Application.config.secret_token = 'e3e879ff3177427a320fcb8656058df7ee5e3dced5a5c60fa752d5b78eced10bf3f0a067da5ebd65f1bdf226575d934ee69a8ad6d5badfffeeb960bdf918ed7e'
